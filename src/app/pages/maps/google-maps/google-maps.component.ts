826e050fb054df1d97d919ad26fbf1311f02d9f5
Michael Wright <wright.mike@utah.edu>
2019-11-16 18:05:11 -0700

    google maps working without AGM. Should be able to do more complex stuff with polylines and heatmaps more easily

--- src/app/pages/maps/google-maps/google-maps.component.ts
+++ src/app/pages/maps/google-maps/google-maps.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild} from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit} from "@angular/core";
 import { LocationDataService } from './location-data.service';
 
 @Component({
@@ -6,14 +6,33 @@ import { LocationDataService } from './location-data.service';
   templateUrl: './google-maps.component.html',
   styleUrls: ['./google-maps.component.scss']
 })
-export class GoogleMapsComponent implements OnInit {
+export class GoogleMapsComponent implements OnInit, AfterViewInit {
+  
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+  map: google.maps.Map;
+
   lat: number = 40.730610;
   lng: number = -73.935242;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
   mapType: string = 'terrain';
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8,
+  };
 
   constructor(private locationService: LocationDataService) {}
 
   ngOnInit() {
+    
   }
+
+  ngAfterViewInit(): void {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, 
+    this.mapOptions);
+   }
 
 }
